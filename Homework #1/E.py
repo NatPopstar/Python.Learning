6e634e1e3d237c4e6b16516f8193148761083365
Nat Popstar <nat.popstar@gmail.com>
2021-07-03 18:23:46 +0300

    Add New Repos

---
+++ Homework #1/E.py
@@ -0,0 +1,7 @@
+"""На вход подается два целых числа, вычислить расстояние между ними на числовой оси."""
+
+#a = int(input())
+#b = int(input())
+#result = abs(a-b)
+#print(result)
+print(abs(int(input()) - int(input())))
