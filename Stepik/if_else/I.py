9867a98a9424a1dfcf8c67874dbdf38735a8e2f4
Nat Popstar <nat.popstar@gmail.com>
2021-07-16 20:46:40 +0300

    Add

---
+++ Stepik/if_else/I.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая считывает три числа и подсчитывает сумму только положительных чисел.
+# put your python code here
+a = int(input())
+b = int(input())
+c = int(input())
+d = 0
+
+if a > 0:
+	d = d + a
+if b > 0:
+   d = d + b
+if c > 0:
+	d = d + c
+print(d)
